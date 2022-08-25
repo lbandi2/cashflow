a7d3b253c06b6bbfb93e1a7d4d3d9737d4474eac
Sergio Schmilovich <sergio.schmilovich@gmail.com>
2022-08-25 14:42:33 -0500

    Cosmetic updates

--- proj/accounts/forms.py
+++ proj/accounts/forms.py
@@ -1,12 +1,11 @@
 from django.forms import ModelForm
 
-from cashflow.models import OperationAccount, OperationCategories, AccountCategories
+from cashflow.models import OperationAccount, OperationCategories
 
 from django import forms
 
 
-CATEGORIES = sorted({(item.name, item.name) for item in OperationCategories.objects.all()})
-ACCOUNT_CATEGORIES = sorted({(item.name, item.name) for item in AccountCategories.objects.all()})
+CATEGORIES = {(item.name, item.name) for item in OperationCategories.objects.all()}
 
 
 class EditOpForm(ModelForm):
@@ -14,7 +13,7 @@ class EditOpForm(ModelForm):
     type = forms.CharField(label='Tipo', widget=forms.TextInput(attrs={'readonly':'readonly'}))
     amount = forms.FloatField(label='Monto', widget=forms.TextInput(attrs={'readonly':'readonly'}))
     entity = forms.CharField(label='Entidad', widget=forms.TextInput(attrs={'readonly':'readonly'}))
-    category = forms.CharField(label='Categoría', max_length=20, widget=forms.Select(choices=sorted(CATEGORIES + ACCOUNT_CATEGORIES)))
+    category = forms.CharField(label='Categoría', max_length=20, widget=forms.Select(choices=sorted(CATEGORIES)))
 
     class Meta:
         model = OperationAccount
--- proj/cashflow/models.py
+++ proj/cashflow/models.py
@@ -119,9 +119,3 @@ class OperationCategories(models.Model):
 
     def __str__(self):
         return self.name
-
-class AccountCategories(models.Model):
-    name = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.name
