0414c2926f735858c26f1449eeace88266530f28
Sergio Schmilovich <sergio.schmilovich@gmail.com>
2022-12-23 10:39:44 -0500

    test

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -147,9 +147,9 @@ STATIC_URL = 'static/'
 # STATIC_ROOT = '/home/sergio/scripts/cashflow/proj/static'
 STATIC_ROOT = BASE_DIR / 'static'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-    ]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+#     ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
